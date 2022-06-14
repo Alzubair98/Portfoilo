551fd78f8bc62e49589a468823a3557e091e08e7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-14 17:06:50 +0300

    adding js linters& add the js code

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-const hamburger = document.querySelector(".button1")
-const navMenu = document.querySelector(".orders")
-const cross = document.querySelector(".x-button") 
-const link = document.querySelectorAll(".nav-link")
+const hamburger = document.querySelector('.button1');
+const navMenu = document.querySelector('.orders');
+const cross = document.querySelector('.x-button');
+const link = document.querySelectorAll('.nav-link');
+let i = 0;
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  cross.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-hamburger.addEventListener("click", function(){
-    hamburger.classList.toggle("active");
-    cross.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    
-} ) 
+cross.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  cross.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-cross.addEventListener("click", function(){
-    hamburger.classList.toggle("active");
-    cross.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    
-} ) 
-
-
-
-for (var i=0; i < link.length; i++){
-    link[i].addEventListener("click",function(){
-        hamburger.classList.toggle("active");
-        cross.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    })
+for (i = 0; i < link.length; i += 1) {
+  link[i].addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    cross.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
 }
