f9b3a2e8979f40563adcaedf87b0b1a77607026b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-14 19:08:38 +0300

    adding blur to the background

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const hamburger = document.querySelector('.button1');
 const navMenu = document.querySelector('.orders-cont');
 const cross = document.querySelector('.x-button');
 const link = document.querySelectorAll('.nav-link');
+const section = document.querySelector(".section-2")
+
+
 let i = 0;
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  section.classList.toggle('active');
 });
 
 cross.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  section.classList.toggle('active');
 });
 
 for (i = 0; i < link.length; i += 1) {
   link[i].addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
+    section.classList.toggle('active');
   });
 }
