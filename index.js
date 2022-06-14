19f0d4e6fd8352dcba65e7124084869348374b6e
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-14 18:36:47 +0300

    fix the menu

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const hamburger = document.querySelector('.button1');
-const navMenu = document.querySelector('.orders');
+const navMenu = document.querySelector('.orders-cont');
 const cross = document.querySelector('.x-button');
 const link = document.querySelectorAll('.nav-link');
 let i = 0;
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  cross.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
 cross.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  cross.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
 for (i = 0; i < link.length; i += 1) {
   link[i].addEventListener('click', () => {
     hamburger.classList.toggle('active');
-    cross.classList.toggle('active');
     navMenu.classList.toggle('active');
   });
 }
