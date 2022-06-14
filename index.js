53d079dfe0230f4af0c2961c2d46e9ab3cecf900
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-14 23:55:41 +0300

    adding blur to all elements

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const navMenu = document.querySelector('.orders-cont');
 const cross = document.querySelector('.x-button');
 const link = document.querySelectorAll('.nav-link');
 const section = document.querySelector('.section-2');
+const continer = document.querySelector('.header');
+const section3 = document.querySelector('.section-3');
+const section4 = document.querySelector('.desk-continer');
+const section5 = document.querySelector('footer');
 
 let i = 0;
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   section.classList.toggle('active');
+  continer.classList.toggle('active');
+  section3.classList.toggle('active');
+  section4.classList.toggle('active');
+  section5.classList.toggle('active');
 });
 
 cross.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   section.classList.toggle('active');
+  continer.classList.toggle('active');
+  section3.classList.toggle('active');
+  section4.classList.toggle('active');
+  section5.classList.toggle('active');
 });
 
 for (i = 0; i < link.length; i += 1) {
@@ -22,5 +34,9 @@ for (i = 0; i < link.length; i += 1) {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
     section.classList.toggle('active');
+    continer.classList.toggle('active');
+    section3.classList.toggle('active');
+    section4.classList.toggle('active');
+    section5.classList.toggle('active');
   });
 }
