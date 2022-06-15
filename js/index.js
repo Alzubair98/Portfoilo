98cfe0e9858c2c599302369a505bb7f6bc9fc7f1
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-15 23:29:24 +0300

    fixing the popup card

--- js/index.js
+++ js/index.js
@@ -42,3 +42,181 @@ for (i = 0; i < link.length; i += 1) {
   });
 }
 
+
+
+
+
+
+
+// cards-section
+
+const cardsSection = `
+<section class="section-3">
+      
+      <div class="card" id="cards">
+        <div class="photo">
+          <img
+            class="work-img"
+            src="Microverse-Student-Project-1/img2.png"
+            alt="card-img"
+          />
+        </div>
+        <div class="left-block">
+          <div class="primary-text">
+            <h2 class="project-title">Tonic</h2>
+            <div class="frame-2">
+              <div class="Client">
+                <h2 class="CANPOY">CANPOY</h2>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">Back End Dev</h2></div>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">2022</h2></div>
+              </div>
+            </div>
+          </div>
+          <div class="information">
+            <p class="des-text">
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required.
+            </p>
+          </div>
+
+          <ul class="tags">
+            <li class="tag"><a class="html" href="">html</a></li>
+            <li class="tag"><a class="html" href="">CSS</a></li>
+            <li class="tagj"><a class="html" href="">JavaScript</a></li>
+          </ul>
+
+          <div class="action">
+            <button class="normal" type="button">See Project</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="photo">
+          <img
+            class="work-img work-img1"
+            src="Microverse-Student-Project-1/img3.png"
+            alt="card-img"
+          />
+        </div>
+        <div class="left-block left-block1">
+          <div class="primary-text">
+            <h2 class="project-title">Multi-Post Stories</h2>
+            <div class="frame-2">
+              <div class="Client">
+                <h2 class="CANPOY">CANPOY</h2>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">Back End Dev</h2></div>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">2022</h2></div>
+              </div>
+            </div>
+          </div>
+          <div class="information">
+            <p class="des-text">
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required.
+            </p>
+          </div>
+
+          <ul class="tags">
+            <li class="tag"><a class="html" href="">html</a></li>
+            <li class="tag"><a class="html" href="">CSS</a></li>
+            <li class="tagj"><a class="html" href="">JavaScript</a></li>
+          </ul>
+
+          <div class="action">
+            <button class="normal" type="button">See Project</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="photo">
+          <img
+            class="work-img"
+            src="Microverse-Student-Project-1/img4.png"
+            alt="card-img"
+          />
+        </div>
+        <div class="left-block">
+          <div class="primary-text">
+            <h2 class="project-title">Tonic</h2>
+            <div class="frame-2">
+              <div class="Client">
+                <h2 class="CANPOY">CANPOY</h2>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">Back End Dev</h2></div>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">2022</h2></div>
+              </div>
+            </div>
+          </div>
+          <div class="information">
+            <p class="des-text">
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required.
+            </p>
+          </div>
+
+          <ul class="tags">
+            <li class="tag"><a class="html" href="">html</a></li>
+            <li class="tag"><a class="html" href="">CSS</a></li>
+            <li class="tagj"><a class="html" href="">JavaScript</a></li>
+          </ul>
+
+          <div class="action">
+            <button class="normal" type="button">See Project</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="photo">
+          <img
+            class="work-img work-img1"
+            src="Microverse-Student-Project-1/img5.png"
+            alt="card-img"
+          />
+        </div>
+        <div class="left-block left-block1">
+          <div class="primary-text">
+            <h2 class="project-title">Multi-Post Stories</h2>
+            <div class="frame-2">
+              <div class="Client">
+                <h2 class="CANPOY">CANPOY</h2>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">Back End Dev</h2></div>
+                <i class="counter fa-solid fa-circle"></i>
+                <div class="Role"><h2 class="back-end">2022</h2></div>
+              </div>
+            </div>
+          </div>
+          <div class="information">
+            <p class="des-text">
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required.
+            </p>
+          </div>
+
+          <ul class="tags">
+            <li class="tag"><a class="html" href="">html</a></li>
+            <li class="tag"><a class="html" href="">CSS</a></li>
+            <li class="tagj"><a class="html" href="">JavaScript</a></li>
+          </ul>
+
+          <div class="action">
+            <button class="normal" type="button">See Project</button>
+          </div>
+        </div>
+      </div>
+    </section>
+`
+
+const divi = document.createElement("div");
+
+divi.innerHTML = cardsSection
+
+document.getElementById("section2").after(divi)
